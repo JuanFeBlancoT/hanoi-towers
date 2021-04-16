4ccf0fc5afe54abf1d0ff9c734a627c935ba03ee
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-04-15 23:22:05 -0500

    Only works with 1 and 2 discs

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static int towers[] = new int[3];
 	
 	public static void main(String[] args) {
 		
@@ -32,28 +33,50 @@ public class Main {
 		
 	}
 
-	 private static void hanoiTowers(int initN, int n, int a, int b, int c) {
+	 private static int[] hanoiTowers(int initN, int n, int a, int b, int c) {
 		
+		towers[0] = a;
+		towers[1] = b;
+		towers[2] = c;
+		 
 		if(n==1 && initN%2 != 0) {
-			a--;
-			c++;
-			System.out.println(a+" "+b+" "+c);
-			return;
-		
+			//System.out.println(towers[0]+":"+towers[2]);
 			
+			towers[0]--;
+			towers[2]++;
+			System.out.println(towers[0]+" "+towers[1]+" "+towers[2]+ "  BBB");
+			return towers;
 			
-		}else if(n==1 && initN%2 != 0) {
-			a--;
-			c++;
-			System.out.println(a+" "+b+" "+c);
-			return;
-		}else {
-			hanoiTowers(initN,n-1,a,c,b);
+		}else if(n!=1 && initN%2 != 0) {
 			
-			System.out.println(a+" "+b+" "+c);
+			hanoiTowers(initN,n-1,towers[0],towers[1],towers[2]);
 			
-			hanoiTowers(initN,n-1,b,a,c);
+			towers[0]--;
+			towers[2]++;
+			System.out.println(towers[0]+" "+towers[1]+" "+towers[2]);
+			
+			hanoiTowers(initN-1,n-1,towers[1],towers[0],towers[2]);
+			
+		}else if(n==1 && initN%2 == 0) {
+			//System.out.println(towers[0]+":"+towers[1]);
+			towers[0]--;
+			towers[1]++;
+			System.out.println(towers[0]+" "+towers[1]+" "+towers[2]+"  AAA");
+			return towers;
+			
+		}else if(n!=1 && initN%2 == 0) {
+			
+			hanoiTowers(initN,n-1,towers[0],towers[1],towers[2]);
+			
+			towers[0]--;
+			towers[2]++;
+			System.out.println(towers[0]+" "+towers[1]+" "+towers[2]);
+			
+			hanoiTowers(initN-1,n-1,towers[1],towers[0],towers[2]);
+			
 		}
+			
+			return towers;
 	}
 
 }
